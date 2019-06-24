1477551707508bb290d7281e3c71b28be343f04d
Jobith M Basheer <jobithmbasheer@gmail.com>
2019-06-24 13:04:49 +0530

    bug-fixes

--- background.js
+++ background.js
@@ -19,7 +19,7 @@ chrome.runtime.onInstalled.addListener(function() {
 			}
 		})
 		chrome.storage.sync.get('username', function(val) {
-			if(val && val.username == data.sender.username)
+			if(val && val.username == data.receiver.username)
 				chrome.notifications.create(
 					`ping user ${data.sender.username} to ${data.receiver.username}`,
 					{   
